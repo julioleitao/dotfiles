cc57e99b7db06dffc3c62e595f93762bf2e583f5
Júlio Leitão <julio.leitao.jr@gmail.com>
2018-11-14 09:52:54 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,12 @@
 
 HOME=/Users/$(whoami)
 
-# TODO colocar num array os arquivos de interesse: bash_profile, vimrc, tmuxconfig. gitconfig, fish_config
+# TODO colocar num array os arquivos de interesse: bash_profile, vimrc, tmuxconfig. gitconfig, fish_config, iterm
+# TODO tem que conferir se tenho dado sensível.
 
 cp $HOME/.vimrc .
 
+# TODO git diff e confirmacao
 git add .
 git commit -m 'Updating the dot files'
 git push
