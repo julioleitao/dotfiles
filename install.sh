0edacaaa55575785c24bf53b7e761816f2245c45
Júlio Leitão <julio.leitao.jr@gmail.com>
2018-11-28 11:50:46 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-VUNDLE_DIR="~/.vim/bundle/Vundle.vim"
+VUNDLE_DIR="$HOME/.vim/bundle/Vundle.vim"
 
 if [ ! -d "$VUNDLE_DIR" ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
+  git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
 fi
 
 curl -s https://raw.githubusercontent.com/julioleitao/dotfiles/master/install.sh > ~/.vimrc
